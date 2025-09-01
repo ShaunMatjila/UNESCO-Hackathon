5ba15772735dcbcc784770c45dc99f0cc62fddfd
shaunmat <shaunm@pulego.co.za>
2025-09-01 20:07:55 +0200

    hot fire fixes

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { CheckCircle, XCircle, ArrowRight, BookOpen, Target, Trophy, Clock } from 'lucide-react';
 
 const Test = () => {
